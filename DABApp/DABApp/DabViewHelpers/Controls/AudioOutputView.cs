e8d31c137ec8c751936388298b9f78f5d709b8bc
Victor Finney <finneyv09@gmail.com>
2017-02-07 15:35:32 -0500

    Fixed another problem with merge.

---
+++ DABApp/DABApp/DabViewHelpers/Controls/AudioOutputView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace DABApp
+{
+	public class AudioOutputView: View
+	{
+		public AudioOutputView()
+		{
+		}
+
+		//public static readonly BindableProperty RoutesProperty = BindableProperty.Create(propertyName:"Routes", returnType:typeof(string), 
+		//                                                                                 declaringType:typeof(Button), defaultValue:);
+	}
+}
