12ae888c242c2ed21f67a38e9dfce627a0879fcd
Victor Finney <finneyv09@gmail.com>
2017-02-09 15:18:50 -0500

    DABMA-80 Got the menu on the base page to work.

--- DABApp/iOS/CustomRenderers/DabBaseContentPageRenderer.cs
+++ DABApp/iOS/CustomRenderers/DabBaseContentPageRenderer.cs
@@ -34,6 +34,44 @@ namespace DABApp.iOS
 			new SlideOverKitiOSHandler().Init(this);
 		}
 
+		 protected override void OnElementChanged (VisualElementChangedEventArgs e)
+        {
+            base.OnElementChanged (e);
+
+            if (OnElementChangedEvent != null)
+                OnElementChangedEvent (e);
+        }
+
+        public override void ViewDidLayoutSubviews ()
+        {
+            base.ViewDidLayoutSubviews ();
+            if (ViewDidLayoutSubviewsEvent != null)
+                ViewDidLayoutSubviewsEvent ();
+
+        }
+
+        public override void ViewDidAppear (bool animated)
+        {
+            base.ViewDidAppear (animated);
+            if (ViewDidAppearEvent != null)
+                ViewDidAppearEvent (animated);
+
+        }
+
+        public override void ViewDidDisappear (bool animated)
+        {
+            base.ViewDidDisappear (animated);          
+            if (ViewDidDisappearEvent != null)
+                ViewDidDisappearEvent (animated);
+        }
+
+        public override void ViewWillTransitionToSize (CGSize toSize, IUIViewControllerTransitionCoordinator coordinator)
+        {
+            base.ViewWillTransitionToSize (toSize, coordinator);
+            if (ViewWillTransitionToSizeEvent != null)
+                ViewWillTransitionToSizeEvent (toSize, coordinator);            
+        }
+
 		public override void ViewWillAppear(bool animated)
 		{
 			base.ViewWillAppear(animated);
