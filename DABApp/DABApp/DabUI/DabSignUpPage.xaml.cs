38b24dacaffa411bdf44477806c9d44c480e612a
Victor Finney <finneyv09@gmail.com>
2017-03-06 13:39:15 -0500

    Fixed Terms of Service button by making it a HtmlLabel and adding a tap gesture recognizer to it.

--- DABApp/DABApp/DabUI/DabSignUpPage.xaml.cs
+++ DABApp/DABApp/DabUI/DabSignUpPage.xaml.cs
@@ -12,20 +12,30 @@ namespace DABApp
 			InitializeComponent();
 			BindingContext = ContentConfig.Instance.blocktext;
 			this.ToolbarItems.Clear();
+			var tapper = new TapGestureRecognizer();
+			tapper.NumberOfTapsRequired = 1;
+			tapper.Tapped += (sender, e) => {
+				Navigation.PushAsync(new DabTermsAndConditionsPage());
+			};
+			Terms.GestureRecognizers.Add(tapper);
+			Terms.Text = "<div style='font-size:14px;'>By signing up I agree to the Daily Audio Bible <span style='color: #ff0000'>Terms of Service.</span></div>";
 		}
 
 		async void OnSignUp(object o, EventArgs e) {
-			if (await AuthenticationAPI.CreateNewMember(FirstName.Text, LastName.Text, Email.Text, Password.Text))
+			if (Agreement.IsToggled)
 			{
-				Navigation.PushModalAsync(new NavigationPage(new DabChannelsPage()));
+				if (await AuthenticationAPI.CreateNewMember(FirstName.Text, LastName.Text, Email.Text, Password.Text))
+				{
+					Navigation.PushModalAsync(new NavigationPage(new DabChannelsPage()));
+				}
+				else
+				{
+					DisplayAlert("Error", "Something broke", "OK");
+				}
 			}
 			else {
-				DisplayAlert("Error", "Something broke", "OK");
+				DisplayAlert("Wait", "Please read and agree to the Daily Audio Bible Terms of Service.", "OK");
 			}
-		}
-
-		void OnTerms(object o, EventArgs e) {
-			Navigation.PushAsync(new DabTermsAndConditionsPage());
 		}
 	}
 }
