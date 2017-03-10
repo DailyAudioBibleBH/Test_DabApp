edc60767fac7cb25c0768fd8e3e8385889442a60
Victor Finney <finneyv09@gmail.com>
2017-03-10 10:49:24 -0500

    Increased the padding inside of the entry for the Android platform.  Unable to get the top and bottom padding to increase on the iOS platform for now.

---
+++ DABApp/Droid/CustomRenderers/DabEntryRenderer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using DABApp.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly:ExportRenderer(typeof(Entry), typeof(DabEntryRenderer))]
+namespace DABApp.Droid
+{
+	public class DabEntryRenderer: EntryRenderer
+	{
+		protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+		{
+			base.OnElementChanged(e);
+			Control.SetPadding(50, 50, 50, 50);
+		}
+	}
+}
---
+++ DABApp/iOS/CustomRenderers/DabEntryRenderer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using CoreGraphics;
+using DABApp.iOS;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly:ExportRenderer(typeof(Entry), typeof(DabEntryRenderer))]
+namespace DABApp.iOS
+{
+	public class DabEntryRenderer: EntryRenderer
+	{
+		protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+		{
+			base.OnElementChanged(e);
+			Control.LeftView = new UIView(new CGRect(2, 0, 2, 0));
+			Control.LeftViewMode = UITextFieldViewMode.Always;
+			Control.RightView = new UIView(new CGRect(2, 0, 2, 0));
+			Control.RightViewMode = UITextFieldViewMode.Always;
+		}
+	}
+}
