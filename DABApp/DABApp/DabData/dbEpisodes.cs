f9544d0b4680b63317c3ab70500cfd2462f56b33
Victor Finney <finneyv09@gmail.com>
2017-03-29 08:58:42 -0400

    DABMA-202 Added dbEpisodes Table to the SQLite database.  Included all the fields defined in the JIRA task description.

---
+++ DABApp/DABApp/DabData/dbEpisodes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using SQLite;
+
+namespace DABApp
+{
+	public class dbEpisodes
+	{
+		[PrimaryKey]
+		[Indexed]
+		public int id { get; set;}
+		public string title { get; set;}
+		public string description { get; set;}
+		public string author { get; set;}
+		[Indexed]
+		public DateTime PubDate { get; set;}
+		[Indexed]
+		public int PubDay { get; set;}
+		[Indexed]
+		public int PubMonth { get; set;}
+		[Indexed]
+		public int PubYear { get; set;}
+		public string url { get; set;}
+		public string read_link { get; set;}
+		public string read_version_tag { get; set;}
+		public string read_version_name { get; set;}
+		[Indexed]
+		public string channel_code { get; set;}
+		public string channel_title { get; set;}
+		public string channel_description { get; set;}
+		public bool is_downloaded { get; set; } = false;
+		public string file_name { get; set;}
+		public string is_listened_to { get; set;}
+		public double start_time { get; set; } = 0;
+		public double stop_time { get; set; } = 0;
+	}
+}
