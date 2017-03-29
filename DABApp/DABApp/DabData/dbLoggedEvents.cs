d7a4f4bfe3933c0e595f5e5860e4654e5d1975f6
Victor Finney <finneyv09@gmail.com>
2017-03-29 09:38:17 -0400

    DaBMA-265 created the logged Events table

---
+++ DABApp/DABApp/DabData/dbLoggedEvents.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using SQLite;
+
+namespace DABApp
+{
+	public class dbLoggedEvents
+	{
+		[PrimaryKey]
+		[AutoIncrement]
+		public int id { get; set; }
+		[Indexed]
+		public DateTime entity_datetime { get; set; }
+		public string entityType {get; set;}
+		public string entityId { get; set;}
+		public string action { get; set;}
+		public double playerTime { get; set;}
+	}
+}
