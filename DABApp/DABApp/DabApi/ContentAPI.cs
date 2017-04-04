0c8c295f11b4940c24d172475ff5a0b15194bfe5
Victor Finney <finneyv09@gmail.com>
2017-04-04 09:46:43 -0400

    Bound the available offline switch to the channel’s available offline setting by creating a new dbSettings entity that stored all of the resource ids that were made available off line.

--- DABApp/DABApp/DabApi/ContentAPI.cs
+++ DABApp/DABApp/DabApi/ContentAPI.cs
@@ -4,6 +4,7 @@ using SQLite;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Linq;
+using Newtonsoft.Json.Linq;
 
 namespace DABApp
 {
@@ -17,6 +18,7 @@ namespace DABApp
 		public static bool CheckContent() {
 			var ContentSettings = db.Table<dbSettings>().SingleOrDefault(x => x.Key == "ContentJSON");
 			var DataSettings = db.Table<dbSettings>().SingleOrDefault(x => x.Key=="data");
+			var OfflineSettings = db.Table<dbSettings>().SingleOrDefault(x => x.Key =="AvailableOffline");
 			try{
 				var client = new System.Net.Http.HttpClient();
 				var result = client.GetAsync("https://feed.dailyaudiobible.com/wp-json/lutd/v1/content?" + Guid.NewGuid().ToString()).Result; //Appended the GUID to avoid caching.
@@ -62,7 +64,38 @@ namespace DABApp
 
 		public static void ParseContent(string jsonOut)
 		{
+			var OfflineSettings = db.Table<dbSettings>().SingleOrDefault(x => x.Key == "AvailableOffline");
 			ContentConfig.Instance = JsonConvert.DeserializeObject<ContentConfig>(jsonOut);
+			if (OfflineSettings == null)
+			{
+				OfflineSettings = new dbSettings();
+				OfflineSettings.Key = "AvailableOffline";
+				OfflineSettings.Value = new JArray().ToString();
+				db.Insert(OfflineSettings);
+			}
+			else {
+				List<int> ids = JsonConvert.DeserializeObject<List<int>>(OfflineSettings.Value);
+				List<Resource> resources = ContentConfig.Instance.views.Single(x => x.title == "Channels").resources.Where(x => ids.Contains(x.id)).ToList();
+				foreach (var item in resources) {
+					item.availableOffline = true;
+				}
+			}
+		}
+
+		public static void UpdateOffline(bool offline, int ResourceId) {
+			var OfflineSettings = db.Table<dbSettings>().Single(x => x.Key == "AvailableOffline");
+			var jsonArray = JArray.Parse(OfflineSettings.Value);
+			if (offline)
+			{
+				jsonArray.Add(ResourceId);
+			}
+			else {
+				if (jsonArray.Contains(ResourceId)) {
+					jsonArray.Remove(ResourceId);
+				}
+			}
+			OfflineSettings.Value = jsonArray.ToString();
+			db.Update(OfflineSettings);
 		}
 	}
 }
--- DABApp/DABApp/DabUI/DabEpisodesPage.xaml.cs
+++ DABApp/DABApp/DabUI/DabEpisodesPage.xaml.cs
@@ -7,14 +7,18 @@ namespace DABApp
 {
 	public partial class DabEpisodesPage : DabBaseContentPage
 	{
+		Resource _resource;
+
 		public DabEpisodesPage(Resource resource)
 		{
 			InitializeComponent();
+			_resource = resource;
 			DabViewHelper.InitDabForm(this);
 			var Episodes = PlayerFeedAPI.GetEpisodeList(resource);
 			EpisodeList.ItemsSource = Episodes;
 			bannerImage.Source = resource.images.backgroundPhone;
 			bannerContent.Text = resource.title;
+			Offline.IsToggled = resource.availableOffline;
 		}
 
 		public void OnEpisode(object o, ItemTappedEventArgs e) 
@@ -22,6 +26,11 @@ namespace DABApp
 			var chosen = (dbEpisodes)e.Item;
 			AudioPlayer.Instance.SetAudioFile(chosen.url);
 			Navigation.PushAsync(new DabPlayerPage(chosen));
+		}
+
+		public void OnOffline(object o, ToggledEventArgs e) {
+			_resource.availableOffline = e.Value;
+			ContentAPI.UpdateOffline(e.Value, _resource.id);
 		}
 	}
 }
