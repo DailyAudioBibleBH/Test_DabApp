d438a7211f70e9ca14f95f98d1097ffb5507a76a
Victor Finney <finneyv09@gmail.com>
2017-04-04 11:46:43 -0400

    Added Month picker to Episodes page.  Month picker updates what episodes are viewable depending on the month.

--- DABApp/DABApp/DabUI/DabEpisodesPage.xaml.cs
+++ DABApp/DABApp/DabUI/DabEpisodesPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using SlideOverKit;
 using Xamarin.Forms;
 
@@ -8,17 +9,24 @@ namespace DABApp
 	public partial class DabEpisodesPage : DabBaseContentPage
 	{
 		Resource _resource;
+		List<dbEpisodes> Episodes;
 
 		public DabEpisodesPage(Resource resource)
 		{
 			InitializeComponent();
 			_resource = resource;
 			DabViewHelper.InitDabForm(this);
-			var Episodes = PlayerFeedAPI.GetEpisodeList(resource);
-			EpisodeList.ItemsSource = Episodes;
+			Episodes = PlayerFeedAPI.GetEpisodeList(resource);
+			//EpisodeList.ItemsSource = Episodes;
 			bannerImage.Source = resource.images.backgroundPhone;
 			bannerContent.Text = resource.title;
 			Offline.IsToggled = resource.availableOffline;
+			var months = Episodes.Select(x => x.PubMonth).Distinct().ToList();
+			foreach (var month in months) {
+				Months.Items.Add(month);
+			}
+			Months.SelectedIndex = 0;
+			EpisodeList.ItemsSource = Episodes.Where(x => x.PubMonth == Months.Items[Months.SelectedIndex]);
 		}
 
 		public void OnEpisode(object o, ItemTappedEventArgs e) 
@@ -31,6 +39,10 @@ namespace DABApp
 		public void OnOffline(object o, ToggledEventArgs e) {
 			_resource.availableOffline = e.Value;
 			ContentAPI.UpdateOffline(e.Value, _resource.id);
+		}
+
+		public void OnMonthSelected(object o, EventArgs e) {
+			EpisodeList.ItemsSource = Episodes.Where(x => x.PubMonth == Months.Items[Months.SelectedIndex]);
 		}
 	}
 }
