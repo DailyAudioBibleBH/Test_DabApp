882f9f9089775a0001c713c5e959d296997b4d3c
Chet Cromer <chetcromer@c2itconsulting.net>
2017-04-07 13:33:22 -0400

    Moved and styled progress bar on player bar

---
+++ DABApp/iOS/CustomRenderers/DabProgressBarRenderer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using CoreGraphics;
+using DABApp.iOS;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(ProgressBar), typeof(DabProgressBarRenderer))]
+namespace DABApp.iOS
+{
+	public class DabProgressBarRenderer : ProgressBarRenderer
+	{
+
+		protected override void OnElementChanged(ElementChangedEventArgs<ProgressBar> e)
+		{
+			base.OnElementChanged(e);
+
+			Control.ProgressTintColor = ((Color)App.Current.Resources["HighlightColor"]).ToUIColor();
+		}
+
+
+	}
+}
