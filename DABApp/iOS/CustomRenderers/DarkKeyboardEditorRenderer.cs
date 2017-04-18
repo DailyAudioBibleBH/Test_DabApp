5be1ad1e3ebebe7bd4c9b73fea07406bcf49c6f4
Victor Finney <finneyv09@gmail.com>
2017-04-18 13:31:11 -0400

    Added Dark Keyboards to both the Entry and Editor UI elements.

---
+++ DABApp/iOS/CustomRenderers/DarkKeyboardEditorRenderer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using DABApp;
+using DABApp.iOS;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly:ExportRenderer(typeof(DarkKeyboardEditor), typeof(DarkKeyboardEditorRenderer))]
+namespace DABApp.iOS
+{
+	public class DarkKeyboardEditorRenderer: EditorRenderer
+	{
+		protected override void OnElementChanged(ElementChangedEventArgs<Editor> e)
+		{
+			base.OnElementChanged(e);
+
+			if (Control != null)
+			{
+				Control.KeyboardAppearance = UIKit.UIKeyboardAppearance.Dark;
+			}
+		}
+	}
+}
---
+++ DABApp/iOS/CustomRenderers/DarkKeyboardEntryRenderer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using DABApp;
+using DABApp.iOS;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly:ExportRenderer(typeof(DarkKeyboardEntry), typeof(DarkKeyboardEntryRenderer))]
+namespace DABApp.iOS
+{
+	public class DarkKeyboardEntryRenderer: EntryRenderer
+	{
+		protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+		{
+			base.OnElementChanged(e);
+
+			if (Control != null) {
+				Control.KeyboardAppearance = UIKit.UIKeyboardAppearance.Dark;
+			}
+		}
+	}
+}
