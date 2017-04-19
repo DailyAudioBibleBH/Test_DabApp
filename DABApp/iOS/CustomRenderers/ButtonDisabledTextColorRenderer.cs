f91e8d846cb76ced48e0afe2e8b33f1d40d874e7
Victor Finney <finneyv09@gmail.com>
2017-04-19 10:11:55 -0400

    DABMA-299 Set the spacing in the Initiatives list to 0 and it solved the problem

---
+++ DABApp/iOS/CustomRenderers/ButtonDisabledTextColorRenderer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using DABApp.iOS;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly:ExportRenderer(typeof(Button), typeof(ButtonDisabledTextColorRenderer))]
+namespace DABApp.iOS
+{
+	public class ButtonDisabledTextColorRenderer: ButtonRenderer
+	{
+		protected override void OnElementChanged(ElementChangedEventArgs<Button> e)
+		{
+			base.OnElementChanged(e);
+
+			if (Control != null)
+			{
+				Control.SetTitleColor(UIColor.FromRGB(187, 187, 187), UIControlState.Focused);
+				Control.SetTitleColor(UIColor.FromRGB(187, 187, 187), UIControlState.Selected);
+			}
+		}
+	}
+}
