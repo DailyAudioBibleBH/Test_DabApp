6785adb8b4af2e5b78f1ff5ef3e4359226671222
Victor Finney <finneyv09@gmail.com>
2017-06-01 16:03:25 -0400

    Made slider color red.

---
+++ DABApp/iOS/CustomRenderers/ColoredSlider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using DABApp.iOS;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(Slider), typeof(ColoredSlider))]
+namespace DABApp.iOS
+{
+	public class ColoredSlider: SliderRenderer
+	{
+		protected override void OnElementChanged(ElementChangedEventArgs<Slider> e)
+		{
+			base.OnElementChanged(e);
+
+			if (e.NewElement == null) return;
+
+			if (Control != null) {
+				Control.MinimumTrackTintColor = UIColor.FromRGB(213, 39, 46);
+				Control.MaximumTrackTintColor = UIColor.FromRGB(213, 39, 46);
+			}
+		}
+	}
+}
