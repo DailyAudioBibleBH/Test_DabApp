04b6e966fc14a02eb1abde8d0356f31f05d827dc
Victor Finney <finneyv09@gmail.com>
2017-06-09 08:29:44 -0400

    Reformatted the Player UI on the Tablet Page

--- DABApp/iOS/CustomRenderers/ColoredMpVolumeView.cs
+++ DABApp/iOS/CustomRenderers/ColoredMpVolumeView.cs
@@ -18,7 +18,15 @@ namespace DABApp.iOS
 
 			MPVolumeView control = new MPVolumeView();
 			control.ShowsVolumeSlider = false;
-			UIImage image = UIImage.FromFile("ic_airplay_white.png");
+			UIImage image = new UIImage();
+			if (Device.Idiom == TargetIdiom.Tablet)
+			{
+				image = UIImage.FromFile("ic_airplay_white_2x.png");
+			}
+			else
+			{
+				image = UIImage.FromFile("ic_airplay_white.png");
+			}
 			control.SetRouteButtonImage(image, UIControlState.Normal);
 			control.SetRouteButtonImage(image, UIControlState.Highlighted);
 			SetNativeControl(control);
