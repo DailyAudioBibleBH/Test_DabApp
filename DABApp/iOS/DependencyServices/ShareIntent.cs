6546a51349c03f70f90fa8704530b0c70c10802c
Victor Finney <finneyv09@gmail.com>
2017-06-09 10:18:31 -0400

    Got the share button to work on the iPad.

--- DABApp/iOS/DependencyServices/ShareIntent.cs
+++ DABApp/iOS/DependencyServices/ShareIntent.cs
@@ -2,6 +2,7 @@
 using DABApp.iOS;
 using Foundation;
 using UIKit;
+using Xamarin.Forms;
 
 [assembly: Xamarin.Forms.Dependency(typeof(ShareIntent))]
 namespace DABApp.iOS
@@ -11,6 +12,18 @@ namespace DABApp.iOS
 		public void OpenShareIntent(string Channelcode, string episodeId)
 		{
 			var activityController = new UIActivityViewController(new NSObject[] { UIActivity.FromObject($"https://player.dailyaudiobible.com/{Channelcode}/{episodeId}") }, null);
+			if (Device.Idiom == TargetIdiom.Tablet)
+			{
+				var popover = activityController.PopoverPresentationController;
+				if (popover != null) {
+					var self = UIApplication.SharedApplication.KeyWindow.RootViewController;
+					popover.SourceView = self.View;
+					var frame = UIScreen.MainScreen.Bounds;
+					frame.Height /= 4;
+					popover.SourceRect = frame;
+					popover.PermittedArrowDirections = UIPopoverArrowDirection.Unknown;
+				}
+			}
 			UIApplication.SharedApplication.KeyWindow.RootViewController.PresentViewController(activityController, true, null);
 		}
 	}
