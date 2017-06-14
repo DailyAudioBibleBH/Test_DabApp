f52759f220fbebc85d903b3ab35d35161be1b478
Victor Finney <finneyv09@gmail.com>
2017-06-14 10:47:59 -0400

    Got the Settings page to display as a Master Details page on the Tablet.

--- DABApp/DABApp/DabUI/DabTabletSettingsPage.xaml.cs
+++ DABApp/DABApp/DabUI/DabTabletSettingsPage.xaml.cs
@@ -8,6 +8,7 @@ namespace DABApp
 	public partial class DabTabletSettingsPage : MasterDetailMenuPage
 	{
 		ControlTemplate playerBarTemplate;
+		bool needRemove = true;
 
 		public DabTabletSettingsPage()
 		{
@@ -16,9 +17,9 @@ namespace DABApp
 			NavigationPage.SetTitleIcon(this, "navbarlogo.png");
 			this.SlideMenu = new DabMenuView();
 			SettingsPage.ControlTemplate = playerBarTemplate;
-			AppInfoPage.ControlTemplate = playerBarTemplate;
-			SettingsPage.ToolbarItems.Clear();
+			//AppInfoPage.ControlTemplate = playerBarTemplate;
 			AppInfoPage.ToolbarItems.Clear();
+			//SettingsPage.ToolbarItems.Clear();
 			SettingsPage.Reset.Clicked += OnReset;
 			SettingsPage.Offline.Clicked += OnOffline;
 			SettingsPage.AppInfo.Clicked += OnAppInfo;
@@ -32,21 +33,29 @@ namespace DABApp
 			var ResetPage = new DabResetListenedToStatusPage();
 			this.Detail = ResetPage;
 			ResetPage.ToolbarItems.Clear();
-			ResetPage.ControlTemplate = playerBarTemplate;
+			//ResetPage.ControlTemplate = playerBarTemplate;
+			Remove();
 		}
 
 		void OnOffline(object o, EventArgs e) {
 			var Offline = new DabOfflineEpisodeManagementPage();
 			this.Detail = Offline;
 			Offline.ToolbarItems.Clear();
-			Offline.ControlTemplate = playerBarTemplate;
+			//Offline.ControlTemplate = playerBarTemplate;
+			Remove();
 		}
 
 		void OnAppInfo(object o, EventArgs e) {
 			var AppInfo = new DabAppInfoPage();
 			this.Detail = AppInfo;
 			AppInfo.ToolbarItems.Clear();
-			AppInfo.ControlTemplate = playerBarTemplate;
+			//AppInfo.ControlTemplate = playerBarTemplate;
+			Remove();
+		}
+
+		void Remove() {
+			if (needRemove) SettingsPage.ToolbarItems.RemoveAt(0);
+			needRemove = false;
 		}
 	}
 }
