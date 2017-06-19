aa0a8de89519e185331b10c7f67e8574f3db1f03
Victor Finney <finneyv09@gmail.com>
2017-06-19 14:24:07 -0400

    Fixed the MPVolumeView again.

--- DABApp/iOS/CustomRenderers/ColoredMpVolumeView.cs
+++ DABApp/iOS/CustomRenderers/ColoredMpVolumeView.cs
@@ -14,26 +14,55 @@ namespace DABApp.iOS
 
 		protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.View> e)
 		{
-			base.OnElementChanged(e);
-
-			if (Control != null)
+			//base.OnElementChanged(e);
+			//if (e.OldElement != null) {
+			//	e.OldElement.PropertyChanged -= OnElementPropertyChanged;
+			//}
+			//if (e.NewElement != null) {
+			//	e.NewElement.PropertyChanged += OnElementPropertyChanged;
+			//}
+			try
 			{
 				MPVolumeView control = new MPVolumeView();
 				control.ShowsVolumeSlider = false;
-				UIImage image = new UIImage();
-				if (Device.Idiom == TargetIdiom.Tablet)
-				{
-					image = UIImage.FromFile("ic_airplay_white_2x.png");
-				}
-				else
-				{
-					image = UIImage.FromFile("ic_airplay_white.png");
-				}
-				control.SetRouteButtonImage(image, UIControlState.Normal);
-				control.SetRouteButtonImage(image, UIControlState.Highlighted);
-				SetNativeControl(control);
+			UIImage image = new UIImage();
+			if (Device.Idiom == TargetIdiom.Tablet)
+			{
+				image = UIImage.FromFile("ic_airplay_white_2x.png");
 			}
+			else
+			{
+				image = UIImage.FromFile("ic_airplay_white.png");
+			}
+			control.SetRouteButtonImage(image, UIControlState.Normal);
+			control.SetRouteButtonImage(image, UIControlState.Highlighted);
+			control.SetRouteButtonImage(image, UIControlState.Disabled);
+			control.SetRouteButtonImage(image, UIControlState.Selected);
+			SetNativeControl(control);
+			}
+			catch (Exception ex) { }
 		}
+
+		//protected override void OnElementPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+		//{
+		//	base.OnElementPropertyChanged(sender, e);
+		//	MPVolumeView control = new MPVolumeView();
+		//	control.ShowsVolumeSlider = false;
+		//	UIImage image = new UIImage();
+		//	if (Device.Idiom == TargetIdiom.Tablet)
+		//	{
+		//		image = UIImage.FromFile("ic_airplay_white_2x.png");
+		//	}
+		//	else
+		//	{
+		//		image = UIImage.FromFile("ic_airplay_white.png");
+		//	}
+		//	control.SetRouteButtonImage(image, UIControlState.Normal);
+		//	control.SetRouteButtonImage(image, UIControlState.Highlighted);
+		//	control.SetRouteButtonImage(image, UIControlState.Disabled);
+		//	control.SetRouteButtonImage(image, UIControlState.Selected);
+		//	SetNativeControl(control);
+		//}
 
 	}
 }
