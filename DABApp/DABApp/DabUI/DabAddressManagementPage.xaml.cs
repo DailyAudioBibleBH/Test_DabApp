4313558916676db316de646de49fc539838b8f01
Victor Finney <finneyv09@gmail.com>
2017-06-23 14:04:50 -0400

    Got the Address management page to look like it does in the documentation.

--- DABApp/DABApp/DabUI/DabAddressManagementPage.xaml.cs
+++ DABApp/DABApp/DabUI/DabAddressManagementPage.xaml.cs
@@ -11,5 +11,14 @@ namespace DABApp
 		{
 			InitializeComponent();
 		}
+
+		void OnBilling(object o, EventArgs e) 
+		{
+		}
+
+		void OnShipping(object o, EventArgs e) 
+		{
+			DisplayAlert("Shipping Functionality Absent", "This has not been implemented yet", "OK");
+		}
 	}
 }
