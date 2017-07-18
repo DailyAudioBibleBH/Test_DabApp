deea115a6996fb28f1c7b0f979b218f76cd1ccf1
Victor Finney <finneyv09@gmail.com>
2017-07-18 14:59:00 -0400

    Made it so that a user has to log in to create a topic or reply and is transferred to the log in page and redirects them to the appropriate creation page once they log in.

--- DABApp/DABApp/DabUI/DabForumPhoneTopicDetails.xaml.cs
+++ DABApp/DABApp/DabUI/DabForumPhoneTopicDetails.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 
 using Xamarin.Forms;
@@ -8,6 +9,8 @@ namespace DABApp
 {
 	public partial class DabForumPhoneTopicDetails : DabBaseContentPage
 	{
+		bool login = false;
+
 		public DabForumPhoneTopicDetails(Topic topic)
 		{
 			InitializeComponent();
@@ -17,6 +20,34 @@ namespace DABApp
 			var month = dateTime.ToString("MMMM");
 			var time = dateTime.TimeOfDay.ToString();
 			DetailsView.last.Text = $"{month} {dateTime.Day}, {dateTime.Year} at {time}";
+			DetailsView.reply.Clicked += OnReply;
+		}
+
+		async void OnReply(object o, EventArgs e)
+		{ 
+			if (GuestStatus.Current.IsGuestLogin)
+			{
+				var choice = await DisplayAlert("Log in required", "You must be logged in to reply.  Would you like to log in?", "Yes", "No");
+				if (choice)
+				{
+					await Navigation.PushModalAsync(new DabLoginPage(true));
+					login = true;
+				}
+			}
+			else 
+			{
+				await Navigation.PushAsync(new DabForumCreateReply());
+			}
+		}
+
+		protected override void OnAppearing()
+		{
+			base.OnAppearing();
+			if (login) 
+			{
+				Navigation.PushAsync(new DabForumCreateReply());
+				login = false;
+			}
 		}
 	}
 }
--- DABApp/DABApp/DabUI/DabForumPhoneTopicList.xaml.cs
+++ DABApp/DABApp/DabUI/DabForumPhoneTopicList.xaml.cs
@@ -7,6 +7,8 @@ namespace DABApp
 {
 	public partial class DabForumPhoneTopicList : DabBaseContentPage
 	{
+		bool login = false;
+
 		public DabForumPhoneTopicList(View view, Forum forum)
 		{
 			InitializeComponent();
@@ -18,8 +20,19 @@ namespace DABApp
 		}
 
 		async void OnPost(object o, EventArgs e) 
-		{ 
-			
+		{
+			if (GuestStatus.Current.IsGuestLogin)
+			{
+				var choice = await DisplayAlert("Log in required", "You must be logged in to make a prayer request.  Would you like to log in?", "Yes", "No");
+				if (choice)
+				{
+					await Navigation.PushModalAsync(new DabLoginPage(true));
+				}
+			}
+			else 
+			{
+				await Navigation.PushAsync(new DabForumCreateTopic());
+			}
 		}
 
 		async void OnTopic(object o, ItemTappedEventArgs e) 
@@ -32,8 +45,18 @@ namespace DABApp
 			var result = await ContentAPI.GetTopic(topic);
 			await Navigation.PushAsync(new DabForumPhoneTopicDetails(result));
 			ContentList.topicList.SelectedItem = null;
-			activity.IsVisible = true;
-			activityHolder.IsVisible = true;
+			activity.IsVisible = false;
+			activityHolder.IsVisible = false;
+		}
+
+		protected override void OnAppearing()
+		{
+			base.OnAppearing();
+			if (login)
+			{
+				Navigation.PushAsync(new DabForumCreateTopic());
+				login = false;
+			}
 		}
 	}
 }
