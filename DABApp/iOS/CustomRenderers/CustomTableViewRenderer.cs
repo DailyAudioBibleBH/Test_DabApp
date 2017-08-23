18a0ee77b3b8514ca6c8bffb3380796954ac78d8
Victor Finney <finneyv09@gmail.com>
2017-08-23 15:42:28 -0400

    Made the separator color black for all table views in the app.

---
+++ DABApp/iOS/CustomRenderers/CustomTableViewRenderer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using DABApp.iOS;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(TableView), typeof(CustomTableViewRenderer))]
+namespace DABApp.iOS
+{
+	public class CustomTableViewRenderer: TableViewRenderer
+	{
+		protected override void OnElementChanged(ElementChangedEventArgs<TableView> e)
+		{
+			base.OnElementChanged(e);
+			if (Control == null)
+				return;
+			var tableView = Control as UITableView;
+			tableView.SeparatorColor = ((Color)App.Current.Resources["EpisodeMenuColor"]).ToUIColor();
+		}
+	}
+}
