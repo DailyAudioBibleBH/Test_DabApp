c8280e67352650ca8f7746a92e481d19279a260d
Victor Finney <finneyv09@gmail.com>
2017-08-24 15:28:30 -0400

    UI tweaks and updates.

--- DABApp/iOS/CustomRenderers/CustomTableViewRenderer.cs
+++ DABApp/iOS/CustomRenderers/CustomTableViewRenderer.cs
@@ -16,6 +16,21 @@ namespace DABApp.iOS
 				return;
 			var tableView = Control as UITableView;
 			tableView.SeparatorColor = ((Color)App.Current.Resources["EpisodeMenuColor"]).ToUIColor();
+			var currentTableView = Element as TableView;
+			tableView.WeakDelegate = new CustomHeaderTableModelRenderer(currentTableView);
+		}
+
+		private class CustomHeaderTableModelRenderer : TableViewModelRenderer
+		{
+			private readonly TableView _TableView;
+			public CustomHeaderTableModelRenderer(TableView model) : base(model)
+			{
+				_TableView = model as TableView;
+			}
+			public override UIView GetViewForHeader(UITableView tableView, nint section)
+			{
+				return base.GetViewForHeader(tableView, section);
+			}
 		}
 	}
 }
