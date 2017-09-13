2761124b01e5ba3d4e24e3d4e30e97f9ebea44c3
Chet Cromer <chetcromer@c2itconsulting.net>
2017-09-13 14:29:04 -0400

    Removed player bar from tablet topic details page.

--- DABApp/DABApp/DabUI/DabForumTopicDetailsView.xaml.cs
+++ DABApp/DABApp/DabUI/DabForumTopicDetailsView.xaml.cs
@@ -4,24 +4,34 @@ using Xamarin.Forms;
 
 namespace DABApp
 {
-	public partial class DabForumTopicDetailsView : ContentView
-	{
-		public Button reply { get; set;}
-		public ListView replies { get; set;}
-		public Label last { get; set;}
+    public partial class DabForumTopicDetailsView : ContentView
+    {
+        public Button reply { get; set; }
+        public ListView replies { get; set; }
+        public Label last { get; set; }
 
-		public DabForumTopicDetailsView()
-		{
-			InitializeComponent();
-			BackgroundColor = (Color)App.Current.Resources["PageBackgroundColor"];
-			ControlTemplate = (ControlTemplate)App.Current.Resources["OtherPlayerPageTemplateWithoutScrolling"];
-			reply = rep;
-			replies = reps;
-			last = LastActivity;
-			if (GuestStatus.Current.IsGuestLogin)
-			{
-				rep.Text = "Log in";
-			}
-		}
-	}
+        public DabForumTopicDetailsView()
+        {
+            InitializeComponent();
+            BackgroundColor = (Color)App.Current.Resources["PageBackgroundColor"];
+            if (Device.Idiom == TargetIdiom.Phone)
+            {
+                //Show the mini player on phones
+                ControlTemplate = (ControlTemplate)App.Current.Resources["OtherPlayerPageTemplateWithoutScrolling"];
+            }
+            else
+            {
+                //Don't show the mini player on tablets
+                ControlTemplate = (ControlTemplate)App.Current.Resources["NoPlayerPageTemplateWithoutScrolling"];
+
+            }
+            reply = rep;
+            replies = reps;
+            last = LastActivity;
+            if (GuestStatus.Current.IsGuestLogin)
+            {
+                rep.Text = "Log in";
+            }
+        }
+    }
 }
