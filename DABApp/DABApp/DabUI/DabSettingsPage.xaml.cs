5e7cf70f9d3d1c705bfe71bbcbcbf486141864f5
Chet Cromer <chetcromer@c2itconsulting.net>
2017-09-13 15:26:06 -0400

    Fixed issue with profile page not getting pushed up on tablet, and profile control spacing.

--- DABApp/DABApp/DabUI/DabSettingsPage.xaml.cs
+++ DABApp/DABApp/DabUI/DabSettingsPage.xaml.cs
@@ -5,146 +5,163 @@ using Xamarin.Forms;
 
 namespace DABApp
 {
-	public partial class DabSettingsPage : DabBaseContentPage
-	{
-		public ViewCell offline { get { return _offline;} }
-		//public ViewCell reset { get { return _reset;} }
-		public ViewCell appInfo { get { return _appInfo;} }
-		public ViewCell profile { get { return _profile;} }
-		public ViewCell addresses { get { return _addresses;} }
-		public ViewCell wallet { get { return _wallet;} }
-		public ViewCell donations { get { return _donations;} }
-		ViewCell _offline;
-		//ViewCell _reset;
-		ViewCell _appInfo;
-		ViewCell _profile;
-		ViewCell _addresses;
-		ViewCell _wallet;
-		ViewCell _donations;
+    public partial class DabSettingsPage : DabBaseContentPage
+    {
+        public ViewCell offline { get { return _offline; } }
+        //public ViewCell reset { get { return _reset;} }
+        public ViewCell appInfo { get { return _appInfo; } }
+        public ViewCell profile { get { return _profile; } }
+        public ViewCell addresses { get { return _addresses; } }
+        public ViewCell wallet { get { return _wallet; } }
+        public ViewCell donations { get { return _donations; } }
+        ViewCell _offline;
+        //ViewCell _reset;
+        ViewCell _appInfo;
+        ViewCell _profile;
+        ViewCell _addresses;
+        ViewCell _wallet;
+        ViewCell _donations;
 
-		public DabSettingsPage()
-		{
-			InitializeComponent();
-			DabViewHelper.InitDabForm(this);
-			NavigationPage.SetHasBackButton(this, false);
-			_offline = Offline;
-			//_reset = Reset;
-			_appInfo = AppInfo;
-			_profile = Profile;
-			_addresses = Addresses;
-			_wallet = Wallet;
-			_donations = Donations;
-			if (GuestStatus.Current.IsGuestLogin)
-			{
-				logOut.Clear();
-				Listening.Clear();
-				Account.Clear();
-			}
-			//if (Device.Idiom == TargetIdiom.Tablet)
-			//{
-			//	ControlTemplate NoPlayerBarTemplate = (ControlTemplate)Application.Current.Resources["PlayerPageTemplateWithoutScrolling"];
-			//	ControlTemplate = NoPlayerBarTemplate;
-			//}
+        public DabSettingsPage()
+        {
+            InitializeComponent();
+            DabViewHelper.InitDabForm(this);
+            NavigationPage.SetHasBackButton(this, false);
+            _offline = Offline;
+            //_reset = Reset;
+            _appInfo = AppInfo;
+            _profile = Profile;
+            _addresses = Addresses;
+            _wallet = Wallet;
+            _donations = Donations;
+            if (GuestStatus.Current.IsGuestLogin)
+            {
+                logOut.Clear();
+                Listening.Clear();
+                Account.Clear();
+            }
+            //if (Device.Idiom == TargetIdiom.Tablet)
+            //{
+            //	ControlTemplate NoPlayerBarTemplate = (ControlTemplate)Application.Current.Resources["PlayerPageTemplateWithoutScrolling"];
+            //	ControlTemplate = NoPlayerBarTemplate;
+            //}
 
-		}
+        }
 
-		async void OnLogOut(object o, EventArgs e)
-		{
-			LogOut.IsEnabled = false;
-			AudioPlayer.Instance.Pause();
-			AudioPlayer.Instance.Unload();
-			var nav = new NavigationPage(new DabLoginPage());
-			nav.SetValue(NavigationPage.BarTextColorProperty, (Color)App.Current.Resources["TextColor"]);
-			if (await AuthenticationAPI.LogOut())
-			{
-				Navigation.PushModalAsync(nav);
-			}
-			else
-			{
-				//await DisplayAlert("OH NO!", "Something went wrong, Sorry.", "OK");
-				Navigation.PushModalAsync(nav);
-			}
-		}
+        async void OnLogOut(object o, EventArgs e)
+        {
+            LogOut.IsEnabled = false;
+            AudioPlayer.Instance.Pause();
+            AudioPlayer.Instance.Unload();
+            var nav = new NavigationPage(new DabLoginPage());
+            nav.SetValue(NavigationPage.BarTextColorProperty, (Color)App.Current.Resources["TextColor"]);
+            if (await AuthenticationAPI.LogOut())
+            {
+                Navigation.PushModalAsync(nav);
+            }
+            else
+            {
+                //await DisplayAlert("OH NO!", "Something went wrong, Sorry.", "OK");
+                Navigation.PushModalAsync(nav);
+            }
+        }
 
-		protected override void OnDisappearing()
-		{
-			base.OnDisappearing();
-			LogOut.IsEnabled = true;
-		}
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            LogOut.IsEnabled = true;
+        }
 
-		void OnOffline(object o, EventArgs e) {
-			if (Device.Idiom == TargetIdiom.Phone) {
-				Navigation.PushAsync(new DabOfflineEpisodeManagementPage());
-			}
-		}
+        void OnOffline(object o, EventArgs e)
+        {
+            if (Device.Idiom == TargetIdiom.Phone)
+            {
+                Navigation.PushAsync(new DabOfflineEpisodeManagementPage());
+            }
+        }
 
-		void OnReset(object o, EventArgs e) {
-			if (Device.Idiom == TargetIdiom.Phone) {
-				Navigation.PushAsync(new DabResetListenedToStatusPage());
-			}
-		}
+        void OnReset(object o, EventArgs e)
+        {
+            if (Device.Idiom == TargetIdiom.Phone)
+            {
+                Navigation.PushAsync(new DabResetListenedToStatusPage());
+            }
+        }
 
-		void OnAppInfo(object o, EventArgs e)
-		{
-			if (Device.Idiom == TargetIdiom.Phone)
-			{
-				Navigation.PushAsync(new DabAppInfoPage());
-			}
-		}
+        void OnAppInfo(object o, EventArgs e)
+        {
+            if (Device.Idiom == TargetIdiom.Phone)
+            {
+                Navigation.PushAsync(new DabAppInfoPage());
+            }
+        }
 
-		async void OnProfile(object o, EventArgs e) {
-			ActivityIndicator activity = ControlTemplateAccess.FindTemplateElementByName<ActivityIndicator>(this, "activity");
-			StackLayout activityHolder = ControlTemplateAccess.FindTemplateElementByName<StackLayout>(this, "activityHolder");
-			activity.IsVisible = true;
-			activityHolder.IsVisible = true;
-			var result = await AuthenticationAPI.GetMember();
-			if (Device.Idiom == TargetIdiom.Phone) {
-				await Navigation.PushAsync(new DabProfileManagementPage());
-			}
-			activity.IsVisible = false;
-			activityHolder.IsVisible = false;
-		}
+        async void OnProfile(object o, EventArgs e)
+        {
+            if (Device.Idiom == TargetIdiom.Phone)
+            {
 
-		void OnAddresses(object o, EventArgs e) {
-			if (Device.Idiom == TargetIdiom.Phone) {
-				Navigation.PushAsync(new DabAddressManagementPage());
-			}
-		}
+                ActivityIndicator activity = ControlTemplateAccess.FindTemplateElementByName<ActivityIndicator>(this, "activity");
+                StackLayout activityHolder = ControlTemplateAccess.FindTemplateElementByName<StackLayout>(this, "activityHolder");
+                activity.IsVisible = true;
+                activityHolder.IsVisible = true;
+                var result = await AuthenticationAPI.GetMember();
+                if (Device.Idiom == TargetIdiom.Phone)
+                {
+                    await Navigation.PushAsync(new DabProfileManagementPage());
+                }
+                activity.IsVisible = false;
+                activityHolder.IsVisible = false;
+            }
+        }
 
-		async void OnWallet(object o, EventArgs e) {
-			if (Device.Idiom == TargetIdiom.Phone) {
-				ActivityIndicator activity = ControlTemplateAccess.FindTemplateElementByName<ActivityIndicator>(this, "activity");
-				StackLayout activityHolder = ControlTemplateAccess.FindTemplateElementByName<StackLayout>(this, "activityHolder");
-				activity.IsVisible = true;
-				activityHolder.IsVisible = true;
-				var result = await AuthenticationAPI.GetWallet();
-				if (result != null)
-				{
-					await Navigation.PushAsync(new DabWalletPage(result));
-				}
-				else{
-					await DisplayAlert("Unable to retrieve Wallet information", "This may be due to a loss of internet connectivity.  Please check your connection and try again.", "OK");
-				}
-				activity.IsVisible = false;
-				activityHolder.IsVisible = false;
-			}
-		}
+        void OnAddresses(object o, EventArgs e)
+        {
+            if (Device.Idiom == TargetIdiom.Phone)
+            {
+                Navigation.PushAsync(new DabAddressManagementPage());
+            }
+        }
 
-		async void OnDonations(object o, EventArgs e) {
-			if (Device.Idiom == TargetIdiom.Phone) {
-				ActivityIndicator activity = ControlTemplateAccess.FindTemplateElementByName<ActivityIndicator>(this, "activity");
-				StackLayout activityHolder = ControlTemplateAccess.FindTemplateElementByName<StackLayout>(this, "activityHolder");
-				activity.IsVisible = true;
-				activityHolder.IsVisible = true;
-				var don = await AuthenticationAPI.GetDonations();
-				if (don != null)
-				{
-					await Navigation.PushAsync(new DabManageDonationsPage(don));
-				}
-				else await DisplayAlert("Unable to get Donation information.", "This may be due to a loss of internet connectivity.  Please check your connection and try again.", "OK");
-				activity.IsVisible = false;
-				activityHolder.IsVisible = false;
-			}
-		}
-	}
+        async void OnWallet(object o, EventArgs e)
+        {
+            if (Device.Idiom == TargetIdiom.Phone)
+            {
+                ActivityIndicator activity = ControlTemplateAccess.FindTemplateElementByName<ActivityIndicator>(this, "activity");
+                StackLayout activityHolder = ControlTemplateAccess.FindTemplateElementByName<StackLayout>(this, "activityHolder");
+                activity.IsVisible = true;
+                activityHolder.IsVisible = true;
+                var result = await AuthenticationAPI.GetWallet();
+                if (result != null)
+                {
+                    await Navigation.PushAsync(new DabWalletPage(result));
+                }
+                else
+                {
+                    await DisplayAlert("Unable to retrieve Wallet information", "This may be due to a loss of internet connectivity.  Please check your connection and try again.", "OK");
+                }
+                activity.IsVisible = false;
+                activityHolder.IsVisible = false;
+            }
+        }
+
+        async void OnDonations(object o, EventArgs e)
+        {
+            if (Device.Idiom == TargetIdiom.Phone)
+            {
+                ActivityIndicator activity = ControlTemplateAccess.FindTemplateElementByName<ActivityIndicator>(this, "activity");
+                StackLayout activityHolder = ControlTemplateAccess.FindTemplateElementByName<StackLayout>(this, "activityHolder");
+                activity.IsVisible = true;
+                activityHolder.IsVisible = true;
+                var don = await AuthenticationAPI.GetDonations();
+                if (don != null)
+                {
+                    await Navigation.PushAsync(new DabManageDonationsPage(don));
+                }
+                else await DisplayAlert("Unable to get Donation information.", "This may be due to a loss of internet connectivity.  Please check your connection and try again.", "OK");
+                activity.IsVisible = false;
+                activityHolder.IsVisible = false;
+            }
+        }
+    }
 }
