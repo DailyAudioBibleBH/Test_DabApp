6ea989bc85689e3f50992c15e64289a0965a1f5e
Victor Finney <finneyv09@gmail.com>
2017-10-03 11:18:08 -0400

    Fixed problem of buttons on Android not having borders when needed.

---
+++ DABApp/Droid/CustomRenderers/DabButtonRenderer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using DABApp.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Button), typeof(DabButtonRenderer))]
+namespace DABApp.Droid
+{
+	public class DabButtonRenderer: ButtonRenderer
+	{
+		protected override void OnDraw(Android.Graphics.Canvas canvas)
+		{
+			base.OnDraw(canvas);
+		}
+
+		protected override void OnElementChanged(ElementChangedEventArgs<Button> e)
+		{
+			base.OnElementChanged(e);
+		}
+	}
+}
