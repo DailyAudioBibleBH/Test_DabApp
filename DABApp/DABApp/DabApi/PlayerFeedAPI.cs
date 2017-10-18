97add4b276d95c1527a61e440cc8197c7c3d1cf6
Victor Finney <finneyv09@gmail.com>
2017-10-18 11:37:31 -0400

    Got Checkmark to reappear on offline episodes page of Android app.

--- DABApp/DABApp/DabApi/PlayerFeedAPI.cs
+++ DABApp/DABApp/DabApi/PlayerFeedAPI.cs
@@ -98,7 +98,6 @@ namespace DABApp
 		}
 
 		public static void UpdateOfflineEpisodeSettings() {
-			Debug.WriteLine("SwitchCell works");
 			var offlineSettings = db.Table<dbSettings>().Single(x => x.Key == "OfflineEpisodes");
 			offlineSettings.Value = JsonConvert.SerializeObject(OfflineEpisodeSettings.Instance);
 			db.Update(offlineSettings);
