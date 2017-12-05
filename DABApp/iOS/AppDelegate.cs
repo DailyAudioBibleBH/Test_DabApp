8002d1462ed21a26500a3f802b9fa4dad7b1332b
finneyv <finneyv09@gmail.com>
2017-12-05 13:28:11 -0500

    Fixed journalling on iOS.

--- DABApp/iOS/AppDelegate.cs
+++ DABApp/iOS/AppDelegate.cs
@@ -11,6 +11,9 @@ using SQLite;
 using UIKit;
 using UserNotifications;
 using Xamarin.Forms;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.Security;
 
 namespace DABApp.iOS
 {
@@ -25,7 +28,10 @@ namespace DABApp.iOS
 			SQLitePCL.raw.sqlite3_config(Convert.ToInt32(SQLite3.ConfigOption.Serialized));
 			SQLitePCL.raw.sqlite3_initialize();
 
-			CachedImageRenderer.Init();
+            //Added this to get journaling to work found it here: https://stackoverflow.com/questions/4926676/mono-https-webrequest-fails-with-the-authentication-or-decryption-has-failed
+            ServicePointManager.ServerCertificateValidationCallback += new RemoteCertificateValidationCallback((sender, certificate, chain, sslPolicyErrors) => { return true; });
+
+            CachedImageRenderer.Init();
 
 			var manager = BITHockeyManager.SharedHockeyManager;
 			manager.Configure("71f3b832d6bc47f3a1f96bbda4669815");
@@ -103,5 +109,63 @@ namespace DABApp.iOS
 		{
 			return UIInterfaceOrientationMask.Portrait;
 		}
-	}
+
+        //More of what was needed to get journaling to work on Android once again found it here: https://stackoverflow.com/questions/4926676/mono-https-webrequest-fails-with-the-authentication-or-decryption-has-failed
+        private static bool RemoteCertificateValidationCallback(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        {
+            //Return true if the server certificate is ok
+            if (sslPolicyErrors == SslPolicyErrors.None)
+                return true;
+
+            bool acceptCertificate = true;
+            string msg = "The server could not be validated for the following reason(s):\r\n";
+
+            //The server did not present a certificate
+            if ((sslPolicyErrors &
+                 SslPolicyErrors.RemoteCertificateNotAvailable) == SslPolicyErrors.RemoteCertificateNotAvailable)
+            {
+                msg = msg + "\r\n    -The server did not present a certificate.\r\n";
+                acceptCertificate = false;
+            }
+            else
+            {
+                //The certificate does not match the server name
+                if ((sslPolicyErrors &
+                     SslPolicyErrors.RemoteCertificateNameMismatch) == SslPolicyErrors.RemoteCertificateNameMismatch)
+                {
+                    msg = msg + "\r\n    -The certificate name does not match the authenticated name.\r\n";
+                    acceptCertificate = false;
+                }
+
+                //There is some other problem with the certificate
+                if ((sslPolicyErrors &
+                     SslPolicyErrors.RemoteCertificateChainErrors) == SslPolicyErrors.RemoteCertificateChainErrors)
+                {
+                    foreach (X509ChainStatus item in chain.ChainStatus)
+                    {
+                        if (item.Status != X509ChainStatusFlags.RevocationStatusUnknown &&
+                            item.Status != X509ChainStatusFlags.OfflineRevocation)
+                            break;
+
+                        if (item.Status != X509ChainStatusFlags.NoError)
+                        {
+                            msg = msg + "\r\n    -" + item.StatusInformation;
+                            acceptCertificate = false;
+                        }
+                    }
+                }
+            }
+
+            //If Validation failed, present message box
+            if (acceptCertificate == false)
+            {
+                msg = msg + "\r\nDo you wish to override the security check?";
+                //          if (MessageBox.Show(msg, "Security Alert: Server could not be validated",
+                //                       MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1) == DialogResult.Yes)
+                acceptCertificate = true;
+            }
+
+            return acceptCertificate;
+        }
+    }
 }
