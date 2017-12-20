5a6a3ab17966c802f7b61f2919c8b0a95793c347
Chet Cromer <chetcromer@c2itconsulting.net>
2017-12-20 00:13:38 -0500

    Updates to get release to not crash with null object exception.

--- DABApp/iOS/CustomRenderers/ColoredMpVolumeView.cs
+++ DABApp/iOS/CustomRenderers/ColoredMpVolumeView.cs
@@ -7,31 +7,31 @@ using UIKit;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
 
-[assembly: ExportRenderer(typeof(AudioOutputView), typeof(ColoredMPVolumeView))]
-namespace DABApp.iOS
-{
-	public class ColoredMPVolumeView : ViewRenderer
-	{
+//[assembly: ExportRenderer(typeof(AudioOutputView), typeof(ColoredMPVolumeView))]
+//namespace DABApp.iOS
+//{
+//	public class ColoredMPVolumeView : ViewRenderer
+//	{
 
-		protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.View> e)
-		{
-			try
-			{
-				MPVolumeView control = new MPVolumeView();
-				control.ShowsVolumeSlider = false;
-				UIImage image = new UIImage();
-				image = UIImage.FromBundle("airplay");
-				control.SetRouteButtonImage(image, UIControlState.Normal);
-				control.SetRouteButtonImage(image, UIControlState.Highlighted);
-				control.SetRouteButtonImage(image, UIControlState.Disabled);
-				control.SetRouteButtonImage(image, UIControlState.Selected);
-				SetNativeControl(control);
-			}
-			catch (Exception ex) 
-			{
-				Debug.WriteLine($"Error in ColoredMPVolumeView iOS Renderer: {ex.Message}");
-			}
-		}
+//		protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.View> e)
+//		{
+//			//try
+//			//{
+//				MPVolumeView control = new MPVolumeView();
+//				control.ShowsVolumeSlider = false;
+//				UIImage image = new UIImage();
+//				image = UIImage.FromBundle("airplay");
+//				control.SetRouteButtonImage(image, UIControlState.Normal);
+//				control.SetRouteButtonImage(image, UIControlState.Highlighted);
+//				control.SetRouteButtonImage(image, UIControlState.Disabled);
+//				control.SetRouteButtonImage(image, UIControlState.Selected);
+//				SetNativeControl(control);
+//			//}
+//			//catch (Exception ex) 
+//			//{
+//			//	Debug.WriteLine($"Error in ColoredMPVolumeView iOS Renderer: {ex.Message}");
+//			//}
+//		}
 
-	}
-}
+//	}
+//}
