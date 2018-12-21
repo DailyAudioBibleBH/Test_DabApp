07b3fbb1dd3522309e9e1c172f8ffd917e81aa41
finneyv <finneyv09@gmail.com>
2018-12-21 14:34:33 -0500

    Fixed stacklayout lack of color on iOS still have problem with segmented control not appearing

---
+++ DABApp/iOS/CustomRenderers/DabStackLayoutRenderer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DABApp.iOS;
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(StackLayout), typeof(DabStackLayoutRenderer))]
+namespace DABApp.iOS
+{
+    public class DabStackLayoutRenderer: VisualElementRenderer<StackLayout>
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<StackLayout> e)
+        {
+            base.OnElementChanged(e);
+            BackgroundColor = BackgroundColor;
+        }
+    }
+}
