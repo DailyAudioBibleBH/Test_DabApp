7d84dd3c78ca8b304fa788714947324958e27d4a
Chet Cromer <chetcromer@c2itconsulting.net>
2019-05-03 10:56:04 -0400

    Cleanup of episode class with old code.

--- DABApp/DABApp/DabData/dbEpisodes.cs
+++ DABApp/DABApp/DabData/dbEpisodes.cs
@@ -39,7 +39,7 @@ namespace DABApp
         public bool has_journal { get; set; }
         public bool progressVisible { get; set; }
 
-
+        [Ignore]
         public string File_extension
         //Extension of the file (always lower case)
         {
@@ -49,6 +49,7 @@ namespace DABApp
             }
         }
 
+        [Ignore]
       public string File_name_local
         {
             get
@@ -75,6 +76,7 @@ namespace DABApp
             }
         }
 
+        [Ignore]
         public string File_name
             //File name used to access the file 
         {
@@ -92,126 +94,5 @@ namespace DABApp
             }
         }
 
-	//	[Ignore]
-	//	public bool downloadVisible { 
-	//		get {
-	//			return is_downloaded;
-	//		}
-	//		set {
-	//			is_downloaded = value;
-	//			OnPropertyChanged("downloadVisible");
-	//		}
-	//	}
-
- //       [Ignore]
- //       public int downloadProgress
- //       {
- //           get {
- //               return progress;
- //           }
- //           set {
- //               progress = value;
- //               OnPropertyChanged("downloadProgress");
- //               OnPropertyChanged("progressVisible");
- //           }
- //       }
-
- //       [Ignore]
- //       public bool progressVisible
- //       {
- //           get {
- //               return noProgress = downloadProgress > -1 && !downloadVisible ? true : false;
- //           }
- //           set {
- //               noProgress = downloadProgress > -1 && !downloadVisible ? true : false;
- //               OnPropertyChanged("progressVisible");
- //           }
- //       }
-
-	//	[Ignore]
-	//	public bool listenedToVisible { 
-	//		get {
-	//			return unTouched = is_listened_to == "listened" ? true : false;
-	//		}
-	//		set{
-	//			unTouched = is_listened_to == "listened" ? true : false;
-	//			OnPropertyChanged("listenedToVisible");
-	//		}
-	//	}
-
-	//	[Ignore]
-	//	public bool favoriteVisible 
-	//	{ 
-	//		get 
-	//		{
-	//			return is_favorite;
-	//		}
-	//		set 
-	//		{
-	//			is_favorite = value;
-	//			OnPropertyChanged("favoriteVisible");
-	//			OnPropertyChanged("favoriteSource");
-	//		}
-	//	}
-
-	//	[Ignore]
-	//	public string favoriteSource
-	//	{ 
-	//		get 
-	//		{
-	//			if (Device.RuntimePlatform == "iOS")
-	//			{
-	//				if (is_favorite)
-	//				{
-	//					return "ic_star_white_3x.png";
-	//				}
-	//				else return "ic_star_border_white_3x.png";
-	//			}
-	//			else {
-	//				return is_favorite ? "ic_star_white.png" : "ic_star_border_white.png";
-	//			}
-	//		}
-	//	}
-
- //       [Ignore]
- //       public string listenedToSource
- //       {
- //           get
- //           {
- //               if (listenedToVisible)
- //               {
- //                   return "ic_check_box_white_3x.png";
- //               }
- //               else return "ic_check_box_outline_blank_white_3x.png";
- //           }
- //       }
-
-	//	[Ignore]
-	//	public bool hasJournalVisible
-	//	{ 
-	//		get 
-	//		{
-	//			return has_journal;
-	//		}
-	//		set {
-	//			has_journal = value;
-	//			OnPropertyChanged("hasJournalVisible");
-	//		}
-	//	}
-
-	//	public event PropertyChangedEventHandler PropertyChanged;
-
-	//	protected virtual void OnPropertyChanged([CallerMemberName]string propertyName = null) { 
-	//		var handler = PropertyChanged;
-	//		if (handler != null)
-	//			handler(this, new PropertyChangedEventArgs(propertyName));
-	//	}
-
-	//	public static event EventHandler<PropertyChangedEventArgs> StaticPropertyChanged = delegate { };
-
-	//	private static void NotifyStaticPropertyChanged(string propertyName)
-	//	{
-	//		StaticPropertyChanged(null, new PropertyChangedEventArgs(propertyName));
-	//	}
 	}
 }
