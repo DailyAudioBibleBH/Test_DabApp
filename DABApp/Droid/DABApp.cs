96f9031c6ada791f656efb496546dfebb201fa59
Chet Cromer <chetcromer@c2itconsulting.net>
2019-05-09 15:32:50 -0400

    Firebase Cloud Messaging startup for Android

--- DABApp/Droid/DABApp.cs
+++ DABApp/Droid/DABApp.cs
@@ -1,32 +1,63 @@
 ﻿using System;
 using Android.App;
 using Android.Content;
+using Android.OS;
 using Android.Runtime;
 using Android.Util;
 using Android.Views;
+using Plugin.FirebasePushNotification;
 
 namespace DABApp.Droid
 {
-	[Application]
-	public class DABApp: Application
-	{
-		public static Context AppContext;
+    [Application]
+    public class DABApp : Application
+    {
+        public static Context AppContext;
 
-		public DABApp(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
-    	{
-
-		}
-
-		public override void OnCreate()
-		{
-			base.OnCreate();
-
-			AppContext = this.ApplicationContext;
-
-			SQLite_Droid.Assets = this.Assets;
+        public DABApp(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
+        {
 
         }
 
-	
-	}
+        public override void OnCreate()
+        {
+            base.OnCreate();
+
+            AppContext = this.ApplicationContext;
+
+            SQLite_Droid.Assets = this.Assets;
+
+            //Firebase Initialization
+            //See this post: https://github.com/CrossGeeks/FirebasePushNotificationPlugin/blob/master/docs/GettingStarted.md
+
+            //Set the default notification channel for your app when running Android Oreo
+            if (Build.VERSION.SdkInt >= Android.OS.BuildVersionCodes.O)
+            {
+                //Change for your default notification channel id here
+                FirebasePushNotificationManager.DefaultNotificationChannelId = "FirebasePushNotificationChannel";
+
+                //Change for your default notification channel name here
+                FirebasePushNotificationManager.DefaultNotificationChannelName = "General";
+            }
+
+
+            //If debug you should reset the token each time.
+#if DEBUG
+            FirebasePushNotificationManager.Initialize(this, true);
+#else
+              FirebasePushNotificationManager.Initialize(this,false);
+#endif
+
+            //Handle notification when app is closed here
+            CrossFirebasePushNotification.Current.OnNotificationReceived += (s, p) =>
+            {
+                Console.WriteLine("Notification received!");
+
+            };
+
+
+        }
+
+
+    }
 }
