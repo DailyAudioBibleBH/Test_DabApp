e4df4cef608fefd4cc4e47347f6d1c2457dccfd8
Carr OConnor <carr.oconnor@outlook.com>
2019-07-18 13:12:32 -0400

    Added tap event to local notification and removed vibration

--- DABApp/Droid/DependencyServices/droidDabNativePlayer.cs
+++ DABApp/Droid/DependencyServices/droidDabNativePlayer.cs
@@ -8,6 +8,8 @@ using TaskStackBuilder = Android.Support.V4.App.TaskStackBuilder;
 using Android.OS;
 using Android.App;
 using Application = Android.App.Application;
+using Java.Lang;
+using Android.Content.Res;
 
 [assembly: Dependency(typeof(DroidDabNativePlayer))]
 namespace DABApp.Droid
@@ -47,7 +49,7 @@ namespace DABApp.Droid
 
             var name = "DAB Notifications";
             var description = "DAB Notification Descriptions";
-            var channel = new NotificationChannel(CHANNEL_ID, name, NotificationImportance.Default)
+            var channel = new NotificationChannel(CHANNEL_ID, name, NotificationImportance.Low)
             {
                 Description = description
             };
@@ -71,25 +73,30 @@ namespace DABApp.Droid
                     // Pass the current button press count value to the next activity:
                     var valuesForActivity = new Bundle();
                     valuesForActivity.PutInt(COUNT_KEY, count);
+                    
+                    // Set up an intent so that tapping the notifications returns to this app:
+                    Intent intent = new Intent(Application.Context, typeof(MainActivity));
+
+                    // Create a PendingIntent; we're only using one PendingIntent (ID = 0):
+                    const int pendingIntentId = 0;
+                    PendingIntent pendingIntent =
+                        PendingIntent.GetActivity(Application.Context, pendingIntentId, intent, PendingIntentFlags.OneShot);
 
                     // When the user clicks the notification, SecondActivity will start up.
-                    //var resultIntent = new Intent(Application.Context, typeof(SecondActivity));
+                    var resultIntent = new Intent(Application.Context, typeof(SecondActivity));
 
                     // Pass some values to SecondActivity:
                     //resultIntent.PutExtras(valuesForActivity);
 
                     // Construct a back stack for cross-task navigation:
-                    //var stackBuilder = TaskStackBuilder.Create(this);
+                    //var stackBuilder = TaskStackBuilder.Create(Application.Context);
                     //stackBuilder.AddParentStack(Class.FromType(typeof(SecondActivity)));
                     //stackBuilder.AddNextIntent(resultIntent);
-
-                    // Create the PendingIntent with the back stack:
-                    //var resultPendingIntent = stackBuilder.GetPendingIntent(0, (int)PendingIntentFlags.UpdateCurrent);
 
                     // Build the notification:
                     var builder = new NotificationCompat.Builder(Application.Context, CHANNEL_ID)
                                   .SetAutoCancel(true) // Dismiss the notification from the notification area when the user clicks on it
-                                                       //.SetContentIntent(resultPendingIntent) // Start up this activity when the user clicks the intent.
+                                  .SetContentIntent(pendingIntent) // Start up this activity when the user clicks the intent.
                                   .SetContentTitle(GlobalResources.playerPodcast.ChannelTitle) // Set the title
                                   .SetNumber(count) // Display the count in the Content Info
                                     .SetSmallIcon(Resource.Drawable.app_icon) // This is the icon to display
@@ -113,7 +120,30 @@ namespace DABApp.Droid
 
 
         }
+    }
 
+    [Activity(Label = "Second Activity")]
+    public class SecondActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
 
+            // Get the count value passed to us from MainActivity:
+            var count = Intent.Extras.GetInt(DroidDabNativePlayer.COUNT_KEY, -1);
+
+            // No count was passed? Then just return.
+            if (count <= 0)
+            {
+                return;
+            }
+
+            // Display the count sent from the first activity:
+            global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(DabChannelsPage));
+            System.Diagnostics.Debug.WriteLine("Notification Clicked");
+            //SetContentView(Resource.Layout.Second);
+            //var txtView = FindViewById<TextView>(Resource.Id.textView1);
+            //txtView.Text = $"You clicked the button {count} times in the previous activity.";
+        }
     }
 }
