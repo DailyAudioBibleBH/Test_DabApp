c7bb4b7e5f6209484eed7be0e0dafc17a2c846c7
Chet Cromer <chetcromer@c2itconsulting.net>
2019-08-07 14:33:55 -0400

    Android native player working.

--- DABApp/Droid/DependencyServices/droidDabNativePlayer.cs
+++ DABApp/Droid/DependencyServices/droidDabNativePlayer.cs
@@ -13,6 +13,9 @@ using Android.Content.Res;
 using Android.Media.Session;
 using Android.Support.V4.Media.Session;
 using Plugin.SimpleAudioPlayer;
+using System.IO;
+using Math = System.Math;
+using Android.Media;
 
 [assembly: Dependency(typeof(DroidDabNativePlayer))]
 namespace DABApp.Droid
@@ -31,10 +34,12 @@ namespace DABApp.Droid
         static readonly int NOTIFICATION_ID = 1000;
         static readonly string CHANNEL_ID = "location_notification";
 
-        DabPlayer player;
+        DabPlayer dabplayer;
 
         public DroidDabNativePlayer()
         {
+            player = new Android.Media.MediaPlayer() { };
+            player.Completion += OnPlaybackEnded;
         }
 
         //Static method to create a single notification channel;
@@ -61,7 +66,7 @@ namespace DABApp.Droid
 
         public void Init(DabPlayer Player, bool IntegrateWithLockScreen)
         {
-            player = Player;
+            dabplayer = Player;
             var mSession = new MediaSessionCompat(Application.Context, "MusicService");
             mSession.SetFlags(MediaSessionCompat.FlagHandlesMediaButtons | MediaSessionCompat.FlagHandlesTransportControls);
             var controller = mSession.Controller;
@@ -72,7 +77,7 @@ namespace DABApp.Droid
                 /* SET UP LOCK SCREEN */
                 CreateNotificationChannel();
 
-                player.EpisodeDataChanged += (sender, e) =>
+                dabplayer.EpisodeDataChanged += (sender, e) =>
                 {
                     // Set up an intent so that tapping the notifications returns to this app:
                     Intent intent = new Intent(Application.Context, typeof(MainActivity));
@@ -84,7 +89,7 @@ namespace DABApp.Droid
                         PendingIntent.GetActivity(Application.Context, firstPendingIntentId, intent, 0);
                     PendingIntent pendingIntent =
                         PendingIntent.GetActivity(Application.Context, pendingIntentId, playPauseIntent, 0);
-                    
+
                     // Build the notification:
                     var builder = new NotificationCompat.Builder(Application.Context, CHANNEL_ID)
                                   .SetStyle(new Android.Support.V4.Media.App.NotificationCompat.MediaStyle()
@@ -100,16 +105,228 @@ namespace DABApp.Droid
 
                     // Finally, publish the notification:
                     var notificationManager = NotificationManagerCompat.From(Application.Context);
-                    notificationManager.Notify(NOTIFICATION_ID, builder.Build());                   
+                    notificationManager.Notify(NOTIFICATION_ID, builder.Build());
                 };
 
-                player.EpisodeProgressChanged += (object sender, EventArgs e) =>
+                dabplayer.EpisodeProgressChanged += (object sender, EventArgs e) =>
                 {
 
                 };
             }
         }
+
+
+        /// Raised when audio playback completes successfully 
+        public event EventHandler PlaybackEnded;
+
+        Android.Media.MediaPlayer player;
+
+        static int index = 0;
+
+        /// Length of audio in seconds
+        public double Duration
+        {
+            get
+            {
+                return player == null ? 0 : ((double)player.Duration) / 1000.0;
+            }
+        }
+
+        /// Current position of audio playback in seconds
+        public double CurrentPosition
+        { get
+            {
+                return player == null ? 0 : ((double)player.CurrentPosition) / 1000.0;
+            }
+        }
+
+
+        /// Playback volume (0 to 1)
+        public double Volume
+        {
+            get {
+                return _volume;
+            }
+            set {
+                SetVolume(_volume = value);
+            }
+        }
+        double _volume = 0.5;
+
+        /// Indicates if the currently loaded audio file is playing
+        public bool IsPlaying
+        { get { return player == null ? false : player.IsPlaying; } }
+
+        /// Indicates if the position of the loaded audio file can be updated
+        public bool CanSeek
+        {
+            get {
+                return player == null ? false : true;
+            }
+        }
+
+        string path;
+
+
+        /// Load wav or mp3 audio file from the iOS Resources folder
+        public bool Load(string path)
+        {
+            player.Reset();
+
+
+            if (path.ToLower().StartsWith("http", StringComparison.CurrentCulture))
+            {
+                //Internet resource
+                player.SetAudioStreamType(Android.Media.Stream.Music);
+                player.SetDataSource(path);
+            }
+            else
+            {
+                //Local file
+                player.SetDataSource(path);
+            }
+
+            return PreparePlayer();
+        }
+
+        bool PreparePlayer()
+        {
+            player?.Prepare();
+
+            return (player == null) ? false : true;
+        }
+
+        void DeletePlayer()
+        {
+            Stop();
+
+            if (player != null)
+            {
+                player.Completion -= OnPlaybackEnded;
+                player.Release();
+                player.Dispose();
+                player = null;
+            }
+
+            DeleteFile(path);
+            path = string.Empty;
+        }
+
+        void DeleteFile(string path)
+        {
+            if (string.IsNullOrWhiteSpace(path) == false)
+            {
+                try
+                {
+                    File.Delete(path);
+                }
+                catch
+                {
+                }
+            }
+        }
+
+        /// Begin playback or resume if paused
+        public void Play()
+        {
+            if (player == null)
+                return;
+
+            if (IsPlaying)
+            {
+                Pause();
+                Seek(0);
+            }
+
+            player.Start();
+        }
+
+        ///<Summary>
+        /// Stop playack and set the current position to the beginning
+        ///</Summary>
+        public void Stop()
+        {
+            if (!IsPlaying)
+                return;
+
+            Pause();
+            Seek(0);
+        }
+
+        ///<Summary>
+        /// Pause playback if playing (does not resume)
+        ///</Summary>
+        public void Pause()
+        {
+            player?.Pause();
+        }
+
+        ///<Summary>
+        /// Set the current playback position (in seconds)
+        ///</Summary>
+        public void Seek(double position)
+        {
+            if (CanSeek)
+                player?.SeekTo((int)position * 1000);
+        }
+
+        ///<Summary>
+        /// Sets the playback volume as a double between 0 and 1
+        /// Sets both left and right channels
+        ///</Summary>
+        void SetVolume(double volume)
+        {
+            volume = Math.Max(0, volume);
+            volume = Math.Min(1, volume);
+
+            player?.SetVolume((float)volume, (float)volume);
+        }
+
+        void OnPlaybackEnded(object sender, EventArgs e)
+        {
+            PlaybackEnded?.Invoke(sender, e);
+
+            //this improves stability on older devices but has minor performance impact
+            if (Android.OS.Build.VERSION.SdkInt < Android.OS.BuildVersionCodes.M)
+            {
+                player.SeekTo(0);
+                player.Stop();
+                player.Prepare();
+            }
+        }
+
+        bool isDisposed = false;
+
+        ///<Summary>
+		/// Dispose SimpleAudioPlayer and release resources
+		///</Summary>
+       	protected virtual void Dispose(bool disposing)
+        {
+            if (isDisposed || player == null)
+                return;
+
+            if (disposing)
+                DeletePlayer();
+
+            isDisposed = true;
+        }
+
+        //~SimpleAudioPlayerImplementation()
+        //{
+        //    Dispose(false);
+        //}
+
+        ///<Summary>
+        /// Dispose SimpleAudioPlayer and release resources
+        ///</Summary>
+        public void Dispose()
+        {
+            Dispose(true);
+
+            GC.SuppressFinalize(this);
+        }
     }
+
 
     [Activity]
     public class SecondActivity : Activity
@@ -148,4 +365,6 @@ namespace DABApp.Droid
             Finish();
         }
     }
+
+
 }
