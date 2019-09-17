067bab602fcad4a16a00d12d6c8d80dd8ce037a8
chet <chetcromer@c2itconsulting.net>
2019-09-17 09:56:58 -0400

    Moved SocketHelper class to shared project

---
+++ DABApp/DABApp/DabSockets/DabJournalSocketHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace DABApp.DabSockets
+{
+    public class DabJournalSocketHelper
+    {
+        public string html { get; set; }
+        public string date { get; set; }
+        public string token { get; set; }
+
+        public DabJournalSocketHelper(string Date, string Token)
+        {
+            date = Date;
+            token = Token;
+        }
+
+        public DabJournalSocketHelper(string Html, string Date, string Token)
+        {
+            html = Html;
+            date = Date;
+            token = Token;
+        }
+    }
+}
+
+
--- DABApp/iOS/DependencyServices/SocketHelper.cs
+++ DABApp/iOS/DependencyServices/SocketHelper.cs
@@ -1,23 +1,23 @@
-﻿using System;
-namespace DABApp.iOS
-{
-	public class SocketHelper
-	{
-		public string html { get; set;} 
-		public string date { get; set;}
-		public string token { get; set;}
+﻿//using System;
+//namespace DABApp.iOS
+//{
+//	public class SocketHelper
+//	{
+//		public string html { get; set;} 
+//		public string date { get; set;}
+//		public string token { get; set;}
 
-		public SocketHelper(string Date, string Token)
-		{
-			date = Date;
-			token = Token;
-		}
+//		public SocketHelper(string Date, string Token)
+//		{
+//			date = Date;
+//			token = Token;
+//		}
 
-		public SocketHelper(string Html, string Date, string Token)
-		{
-			html = Html;
-			date = Date;
-			token = Token;
-		}
-	}
-}
+//		public SocketHelper(string Html, string Date, string Token)
+//		{
+//			html = Html;
+//			date = Date;
+//			token = Token;
+//		}
+//	}
+//}
