2c4e67eba47a186e23b9924b23f9e234f657da76
Carr OConnor <carr.oconnor@outlook.com>
2019-10-18 16:13:27 -0400

    ios Implementation started

---
+++ DABApp/iOS/DabSockets/iosWebSocket.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WebSocketSharp;
+
+using Foundation;
+using UIKit;
+using DABApp.DabSockets;
+using Xamarin.Forms;
+using DABApp.iOS.DabSockets;
+
+[assembly: Dependency(typeof(iosWebSocket))]
+namespace DABApp.iOS.DabSockets
+{
+    class iosWebSocket : IWebSocket
+    {
+
+        bool isConnected = false;
+        WebSocket sock;
+        public event EventHandler<DabSocketEventHandler> DabSocketEvent;
+
+        public iosWebSocket()
+        {
+        }
+
+
+
+        //Returns current connection state of the socket.
+        public bool IsConnected
+        {
+            get
+            {
+                return isConnected;
+            }
+        }
+
+        //Init the socket with a URI and register for events we want to know about.
+        public void Init(string Uri, List<String> events)
+        {
+            //Initialize the socket
+            try
+            {
+                sock = new WebSocket(Uri);
+                sock.OnOpen += (sender, data) => { OnConnect(data); };
+                sock.OnMessage += (sender, data) => { };
+                sock.OnClose += (sender, data) => { OnDisconnect(data); };
+
+                //sock.ConnectAsync();
+                //Set up standard events
+                //sock.On("connect", data => OnConnect(data));
+                //sock.On("disconnect", data => OnDisconnect(data));
+                //sock.On("reconnect", data => OnReconnect(data));
+                //sock.On("reconnecting", data => OnEvent("reconnecting", data)); //Use basic OnEvent since nothing is "done" yet
+
+
+                //Set up custom events requested by the caller
+                //foreach (string s in events)
+                //{
+                //    sock.On(s, data => OnEvent(s, data));
+                //}
+
+
+            }
+            catch (Exception ex)
+            {
+                //Init failed 
+                sock = null;
+                isConnected = false;
+            }
+        }
+
+        private object OnEvent(string s, object data)
+        {
+            //A requested event has fired - notify the calling app so it can handle it.
+
+            //Notify the listener
+            DabSocketEvent?.Invoke(this, new DabSocketEventHandler(s, data.ToString()));
+
+            return data;
+        }
+
+
+        private object OnReconnect(object data)
+        {
+            //Socket has reconnected
+            isConnected = true;
+
+            //Notify the listener
+            DabSocketEvent?.Invoke(this, new DabSocketEventHandler("reconnected", data.ToString()));
+
+            //Return
+            return data;
+        }
+
+        private object OnConnect(object data)
+        {
+            //Socket has connected (1st time probably)
+            isConnected = true;
+            var test = sock;
+            //Notify the listener
+            DabSocketEvent?.Invoke(this, new DabSocketEventHandler("connected", data.ToString()));
+            //Return
+            return data;
+        }
+
+        private object OnDisconnect(object data)
+        {
+            //Socket has disconnected
+            isConnected = false;
+
+            //Notify the listener
+            DabSocketEvent?.Invoke(this, new DabSocketEventHandler("disconnected", data.ToString()));
+
+            //Return
+            return data;
+        }
+
+        public void Disconnect()
+        {
+            //Disconnect the socket
+            if (IsConnected)
+            {
+                sock.Close();
+            }
+        }
+
+        public void Send()
+        {
+            sock.Send("test");
+        }
+
+
+        public void Connect()
+        {
+            //Connect the socket
+            sock.ConnectAsync();
+        }
+
+        public void Emit(string Command, object Data)
+        {
+            //Send data to the socket
+            //sock.Emit(Command, Data);
+        }
+    }
+}
