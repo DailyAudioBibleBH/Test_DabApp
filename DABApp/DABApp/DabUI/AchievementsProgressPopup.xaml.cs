559c2f86ba68d27ea2cde9a7f4c6944f5d520cb2
Carr O'Connor <carroconnor@c2itconsulting.net>
2020-03-17 10:19:30 -0400

    Getting through achievements popup

---
+++ DABApp/DABApp/DabUI/AchievementsProgressPopup.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace DABApp.DabUI
+{
+    public partial class AchievementsProgressPopup : ContentView
+    {
+        public AchievementsProgressPopup()
+        {
+            InitializeComponent();
+        }
+    }
+}
