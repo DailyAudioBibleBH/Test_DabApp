4119cc0ef048e194043ab4fd28bc29de1559a972
Carr O'Connor <carroconnor@c2itconsulting.net>
2020-03-18 09:31:39 -0400

    Guest login changes

--- DABApp/DABApp/App.xaml.cs
+++ DABApp/DABApp/App.xaml.cs
@@ -37,7 +37,14 @@ namespace DABApp
 
                 if (AuthenticationAPI.CheckToken() && versionList == null) //Check to see if the user is logged in.
                 {
-                    MainPage = new NavigationPage(new DabChannelsPage()); //Take to channels page is logged in
+                    if (GuestStatus.Current.IsGuestLogin)
+                    {
+                        MainPage = new NavigationPage(new DabLoginPage()); //Take to login page if guest
+                    }
+                    else
+                    {
+                        MainPage = new NavigationPage(new DabChannelsPage()); //Take to channels page is logged in
+                    }
                 }
                 else
                 {
