280a7c0332d0dfd2fa4a568848532d3d8e0aa762
Carr OConnor <carr.oconnor@outlook.com>
2020-03-19 16:34:11 -0400

    brought in header for achievements, working on building out list and binding data

---
+++ DABApp/DABApp/DabData/Helpers/dabUserBadgeProgress.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DABApp.Helpers
+{
+    public class dabUserBadgeProgress
+    {
+        dbBadges badge;
+        dbBadgeProgress progress;
+        public dabUserBadgeProgress(dbBadges badge, dbBadgeProgress progress)
+        {
+            this.badge = badge;
+            this.progress = progress;
+        }
+    }
+}
--- DABApp/DABApp/DabUI/DabAchievementsPage.xaml.cs
+++ DABApp/DABApp/DabUI/DabAchievementsPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,16 +13,37 @@ namespace DABApp
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class DabAchievementsPage : DabBaseContentPage
 	{
+		View AchievementsView;
+		Resource _resource;
 		public DabAchievementsPage(DABApp.View contentView)
 		{
 			InitializeComponent();
 			NavigationPage.SetHasBackButton(this, true);
+			//Init the form
+			DabViewHelper.InitDabForm(this);
+			AchievementsView = ContentConfig.Instance.views.Single(x => x.id == 132262); //TODO: Find this using a key vs. a specific number
+			BindingContext = AchievementsView;
+			//_resource = AchievementsView.resources[0];
 
 			banner.Source = new UriImageSource
 			{
 				Uri =  new Uri((Device.Idiom == TargetIdiom.Phone ? contentView.banner.urlPhone : contentView.banner.urlTablet)),
 				CacheValidity = GlobalResources.ImageCacheValidity
 			};
+
+			SQLiteConnection db = DabData.database;
+			SQLiteAsyncConnection adb = DabData.AsyncDatabase;//Async database to prevent SQLite constraint errors
+
+			List<dbBadges> dbBadgeList = db.Table<dbBadges>().ToList();
+			List<dbBadgeProgress> dbBadgeProgress = db.Table<dbBadgeProgress>().ToList();
+
+			var breakpoint = "";
+			//List<dbBadges> badgeList = new List<dbBadges>();
+
+			//foreach (var item in dbbadgeList)
+			//{
+			//	badgeList.Add(item);
+			//}
 		}
 	}
 }
