a5acbfb8438a7a6668a99eba1390d8593c84651d
Chet Cromer <chetcromer@c2itconsulting.net>
2020-04-01 15:04:36 -0400

    Added android custom renderer.

---
+++ DABApp/Droid/CustomRenderers/DabAndroidAchievementsProgressBarRenderer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.Remoting.Contexts;
+using DABApp;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(DabAchievementsProgressBar), typeof(DABApp.Droid.DabAndroidAchievementsProgressBarRenderer))]
+namespace DABApp.Droid
+{
+    public class DabAndroidAchievementsProgressBarRenderer: ProgressBarRenderer
+    {
+
+        protected override void OnElementChanged(ElementChangedEventArgs<ProgressBar> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Element == null)
+                return;
+
+            var element = (DabAchievementsProgressBar)Element;
+
+            if (Control != null)
+            {
+                Control.ProgressTintList = Android.Content.Res.ColorStateList.ValueOf(Color.FromRgb(182, 231, 233).ToAndroid()); //Change the color
+                Control.ScaleY = 10; //Changes the height
+
+            }
+        }
+        
+    }
+}
