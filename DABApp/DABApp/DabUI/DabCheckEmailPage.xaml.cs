1a416f46ef3ab367feaa99fb46c17e23ef817903
Carr O'Connor <carroconnor@c2itconsulting.net>
2020-05-14 13:42:55 -0400

    Check email page started

---
+++ DABApp/DABApp/DabUI/DabCheckEmailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace DABApp
+{
+    public partial class DabCheckEmailPage : DabBaseContentPage
+    {
+        public DabCheckEmailPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
