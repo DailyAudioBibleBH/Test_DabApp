3c8b32cd8a9cb1df0864495fc67c3f0dd0978058
Carr O'Connor <carroconnor@c2itconsulting.net>
2020-07-16 14:01:25 -0400

    Font back in nav bar title

--- DABApp/iOS/AppDelegate.cs
+++ DABApp/iOS/AppDelegate.cs
@@ -46,10 +46,11 @@ namespace DABApp.iOS
             //Popup INit
             Rg.Plugins.Popup.Popup.Init();
 
-            //Ios Default Tint Color
+            //Ios Nav bar decoration
             UINavigationBar.Appearance.TintColor = UIColor.FromRGB(203, 203, 203);
             UITextAttributes att = new UITextAttributes();
             att.TextColor = UIColor.FromRGB(203, 203, 203);
+            att.Font = UIFont.FromName("FetteEngD", 25);
             UINavigationBar.Appearance.SetTitleTextAttributes(att);
 
             global::Xamarin.Forms.Forms.Init();
