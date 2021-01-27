fc3efe51f3cdc7b6cef9a94e3906aadca0945725
Carr O'Connor <carroconnor@c2itconsulting.net>
2021-01-27 17:24:44 -0500

    only set reading if episode available - tablet

--- DABApp/DABApp/DabUI/DabTabletPage.xaml.cs
+++ DABApp/DABApp/DabUI/DabTabletPage.xaml.cs
@@ -361,23 +361,26 @@ namespace DABApp
         //Set the reading
         async Task SetReading()
         {
-            Reading reading = await PlayerFeedAPI.GetReading(episode.Episode.read_link);
-            //Update the Reading UI on the main thread after getting data from the API
-            Device.BeginInvokeOnMainThread(() =>
+            if (episode != null)
             {
-                ReadTitle.Text = reading.title;
-                ReadText.Text = reading.text;
-                if (reading.IsAlt)
+                Reading reading = await PlayerFeedAPI.GetReading(episode.Episode.read_link);
+                //Update the Reading UI on the main thread after getting data from the API
+                Device.BeginInvokeOnMainThread(() =>
                 {
-                    AltWarning.IsVisible = true;
-                }
-                else AltWarning.IsVisible = false;
-                if (reading.excerpts != null)
-                {
-                    ReadExcerpts.Text = String.Join(", ", reading.excerpts);
-                }
-                else ReadExcerpts.Text = "";
-            });
+                    ReadTitle.Text = reading.title;
+                    ReadText.Text = reading.text;
+                    if (reading.IsAlt)
+                    {
+                        AltWarning.IsVisible = true;
+                    }
+                    else AltWarning.IsVisible = false;
+                    if (reading.excerpts != null)
+                    {
+                        ReadExcerpts.Text = String.Join(", ", reading.excerpts);
+                    }
+                    else ReadExcerpts.Text = "";
+                });
+            }
         }
 
         protected override void OnSizeAllocated(double width, double height)
