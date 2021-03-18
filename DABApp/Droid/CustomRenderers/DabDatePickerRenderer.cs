1c5ab2f241670e0546d296a0c0e96c6f21c91ba4
Carr O'Connor <carroconnor@c2itconsulting.net>
2021-03-18 11:19:19 -0400

    wheel datepicker implemented for ios

---
+++ DABApp/Droid/CustomRenderers/DabDatePickerRenderer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Android.Content;
+using DABApp.DabViewHelpers.Controls;
+using DABApp.Droid.CustomRenderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(CustomDatePicker), typeof(DabDatePickerRenderer))]
+namespace DABApp.Droid.CustomRenderers
+{
+    public class DabDatePickerRenderer : DatePickerRenderer
+    {
+        public DabDatePickerRenderer(Context context) : base(context)
+        { }
+    }
+}
---
+++ DABApp/iOS/CustomRenderers/DabDatePickerRenderer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using DABApp.DabViewHelpers.Controls;
+using DABApp.iOS;
+using DABApp.iOS.CustomRenderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(DatePicker), typeof(DabDatePickerRenderer))]
+namespace DABApp.iOS
+{
+    class DabDatePickerRenderer : DatePickerRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<DatePicker> e)
+        {
+            base.OnElementChanged(e);
+            if (e.NewElement != null && Control != null)
+            {
+                if (UIDevice.CurrentDevice.CheckSystemVersion(14, 0))
+                {
+                    UIDatePicker picker = (UIDatePicker)Control.InputView;
+                    picker.PreferredDatePickerStyle = UIDatePickerStyle.Wheels;
+                }
+            }
+        }
+    }
+}
